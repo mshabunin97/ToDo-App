e3498b94690efcd8730c56b27fe666d3fcff27fc
Maxim Shabunin <cska.maxim@mail.ru>
2023-07-14 11:54:37 +0300

    first commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState } from 'react';
+
+const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState('');
+
+        const handleSubmit = e => {
+            e.preventDefault();
+
+            if (value.trim() === '') {
+                return;
+            }
+
+           addTodo(value)
+
+           setValue('')
+        }
+
+
+    return (
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input
+                value={value}
+                type="text"
+                className='todo-input'
+                placeholder='What is the task today?'
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button type="submit" className='todo-btn'>Add Task</button>
+        </form>
+    );
+};
+
+export default TodoForm;
